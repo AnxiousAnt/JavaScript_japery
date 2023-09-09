d5a0ab6556ddb93bbb94565b618ed62afac5370b
AnxiousAnt <92052531+AnxiousAnt@users.noreply.github.com>
2023-09-09 19:25:39 +0530

    Defined the goals of the project (slot machine)

---
+++ project.js
@@ -0,0 +1,10 @@
+/*
+Goals (slot_machine):
+1. Deposit some money.
+2. Determine the no1. of lines to bet on.
+3. collect bet amount.
+4. spin the slot machine.
+5. check if user won/lost.
+6. give the user their winnings (or take away their bet if lost).
+7. play again
+*/
