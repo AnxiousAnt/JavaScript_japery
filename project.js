ca2b62162fc50167877899b9a3c8643964069d59
AnxiousAnt <92052531+AnxiousAnt@users.noreply.github.com>
2023-09-09 20:05:03 +0530

    Created the deposit function.

--- project.js
+++ project.js
@@ -8,3 +8,26 @@ Goals (slot_machine):
 6. give the user their winnings (or take away their bet if lost).
 7. play again
 */
+
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+    while (true) {
+        const depositAmount = prompt("Enter a deposit amount: ");
+        const numberDepositAmount = parseFloat(depositAmount);
+
+        if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+            console.log("Invalid deposit amount! try again.\n");
+        }
+        else {
+            return numberDepositAmount;
+        }
+    }
+
+}
+
+const depositAmount = deposit();
+console.log(depositAmount);
+
+
+
